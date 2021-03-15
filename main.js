26dc2a3adc216356ff75a0d463e04a0ef02ad5d4
Adil <izenbard@gmail.com>
2021-03-16 05:56:34 +0600

    push

--- main.js
+++ main.js
@@ -8,18 +8,16 @@ if ('serviceWorker' in navigator) {
         });
 }
 
-console.log(navigator.serviceWorker);
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+    displayNotification();
+});
 
-// Notification.requestPermission(function(status) {
-//     console.log('Notification permission status:', status);
-//     displayNotification();
-// });
-//
-// function displayNotification() {
-//     console.log(navigator);
-//     console.log(navigator.serviceWorker);
-//     navigator.serviceWorker.getRegistration().then(function(reg) {
-//       console.log("reg", reg);
-//       reg.showNotification('Hello world!');
-//     });
-// }
+function displayNotification() {
+    console.log(navigator);
+    console.log(navigator.serviceWorker);
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      console.log("reg", reg);
+      reg.showNotification('Hello world!');
+    });
+}
