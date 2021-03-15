724fafcbe3adc4f23a08b97d7a7504413ff2d99d
Adil <izenbard@gmail.com>
2021-03-16 05:46:44 +0600

    sw

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
-    displayNotification();
-});
-
-function displayNotification() {
-    console.log(navigator);
-    console.log(navigator.serviceWorker);
-    navigator.serviceWorker.getRegistration().then(function(reg) {
-      console.log("reg", reg);
-      reg.showNotification('Hello world!');
-    });
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function(error) {
+            console.log('Service worker registration failed, error:', error);
+        });
 }
+
+console.log(navigator.serviceWorker);
+
+// Notification.requestPermission(function(status) {
+//     console.log('Notification permission status:', status);
+//     displayNotification();
+// });
+//
+// function displayNotification() {
+//     console.log(navigator);
+//     console.log(navigator.serviceWorker);
+//     navigator.serviceWorker.getRegistration().then(function(reg) {
+//       console.log("reg", reg);
+//       reg.showNotification('Hello world!');
+//     });
+// }
