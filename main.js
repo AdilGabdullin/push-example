9a88a619049abe59b6db6084e9f92890744802cf
Adil <izenbard@gmail.com>
2021-03-16 06:09:36 +0600

    push

--- main.js
+++ main.js
@@ -1,23 +1,59 @@
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/push-example/service-worker.js')
+//         .then(function(registration) {
+//             console.log('Registration successful, scope is:', registration.scope);
+//         })
+//         .catch(function(error) {
+//             console.log('Service worker registration failed, error:', error);
+//         });
+// }
+//
+// Notification.requestPermission(function(status) {
+//     console.log('Notification permission status:', status);
+//     displayNotification();
+// });
+//
+// function displayNotification() {
+//     navigator.serviceWorker.getRegistration().then(function(reg) {
+//       reg.showNotification('Hello world!');
+//     });
+// }
+
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/push-example/service-worker.js')
-        .then(function(registration) {
-            console.log('Registration successful, scope is:', registration.scope);
-        })
-        .catch(function(error) {
-            console.log('Service worker registration failed, error:', error);
+    navigator.serviceWorker.register('/push-example/service-worker.js').then(function(reg) {
+        console.log('Service Worker Registered!', reg);
+
+        reg.pushManager.getSubscription().then(function(sub) {
+            if (sub === null) {
+                // Update UI to ask user to register for Push
+                console.log('Not subscribed to push service!');
+            } else {
+                // We have a subscription, update the database
+                console.log('Subscription object: ', sub);
+                subscribeUser();
+            }
+        });
+    })
+        .catch(function(err) {
+            console.log('Service Worker registration failed: ', err);
         });
 }
 
-Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
-    displayNotification();
-});
+function subscribeUser() {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready.then(function(reg) {
 
-function displayNotification() {
-    console.log(navigator);
-    console.log(navigator.serviceWorker);
-    navigator.serviceWorker.getRegistration().then(function(reg) {
-      console.log("reg", reg);
-      reg.showNotification('Hello world!');
-    });
+            reg.pushManager.subscribe({
+                userVisibleOnly: true
+            }).then(function(sub) {
+                console.log('Endpoint URL: ', sub.endpoint);
+            }).catch(function(e) {
+                if (Notification.permission === 'denied') {
+                    console.warn('Permission for notifications was denied');
+                } else {
+                    console.error('Unable to subscribe to push', e);
+                }
+            });
+        })
+    }
 }
