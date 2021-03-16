9f0794de872274f27f83fb1be31517540b8982a8
Adil <izenbard@gmail.com>
2021-03-16 09:31:44 +0600

    token

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 console.log({messaging});
+
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+messaging.getToken({vapidKey: 'BMBzjVIIco_D-BLXizlikmjV3_PWXWEaeVl3bbO7i2XaYPIY1kwaNJihZuuOF9kdxfDRvJXcD4Nva99p4w4vNaE'}).then((currentToken) => {
+    if (currentToken) {
+        console.log({currentToken});
+        // Send the token to your server and update the UI if necessary
+        // ...
+    } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+    }
+}).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+});
